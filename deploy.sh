7948f51061632929b391e2c97e65e3ef1edee94e
Nick Pettazzoni <pettazz@gmail.com>
2016-04-15 20:20:03 -0400

    fix deployscript

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 rm build/index.zip
-zip –Xr build/index.zip *
+zip -R build/index.zip ./* -x build/* -X
 
 aws lambda update-function-code --function-name tvrobot --zip-file fileb://build/index.zip
