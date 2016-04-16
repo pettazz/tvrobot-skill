012f65db2d6ce96c1f7e73c9b9e7b838ed5cf700
Nick Pettazzoni <pettazz@gmail.com>
2016-04-16 01:43:44 -0400

    clean up constructing responses

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var shows = require('./sample-shows');
 
 app.launch(function(req, res) {
     var prompt = 'Sup everybody. I\'m a friendly TvRobot. Beep and whatnot. You can ask me when the next episode of any show is on. For examples, say help. To exit, say that thing.';
+    response.session('alive', 'true');
     res.say(prompt);
     res.shouldEndSession(false, 'What can I find for you?');
 });
@@ -62,19 +63,27 @@ app.intent('NextEpisodeIntent', {
 function handleNextEpisodeResponse(response, showName){
     var result = tvr.getNextEpisode(showName).then(function(result){
         if(result.success){
-            var sayText = 'The next episode of ' + result.data.showName + ',' + result.data.episodeName + ' airs ' + moment(result.data.episodeAirTimestamp).tz('America/New_York').format('dddd, MMMM Do, [at] h:mm a [eastern time]');
+            var showName = result.data.showName;
+            var episodeName = result.data.episodeName;
+            var episodeAirTimestamp = result.data.episodeAirTimestamp;
+
+            var formattedTime = moment(result.data.episodeAirTimestamp)
+                                .tz('America/New_York')
+                                .format('dddd, MMMM Do, [at] h:mm a z');
+
+            var sayText = `The next episode of ${showName}, ${episodeName}, airs ${formattedTime}`;
             var cardTitle = result.data.showName;
-            var cardText = 'Next Episode: ' + result.data.episodeName + ' \n Airs on: ' + moment(result.data.episodeAirTimestamp).tz('America/New_York').format('dddd, MMMM Do YYYY, h:mm a z');
+            var cardText = `Next Episode: ${episodeName} \nAirs on: ${formattedTime}`;
 
             response.say(sayText);
             response.card(cardTitle, cardText);
-            if (response.session('open_session') === 'true'){
+            if (response.session('alive') === 'true'){
                 response.shouldEndSession(false);
             }
             response.send();
         }else{
             response.say(result.data.message);
-            if (response.session('open_session') === 'true'){
+            if (response.session('alive') === 'true'){
                 response.shouldEndSession(false);
             }
             response.send();
